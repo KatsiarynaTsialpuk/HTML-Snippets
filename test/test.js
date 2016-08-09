226c87bb273a3b2a202cbbbbe72f2a956f9a75a2
code-mattclaffey <matt.claffey@codecomputerlove.com>
2016-08-09 16:14:44 +0100

    Feat: added more compelxity around the testing

---
+++ test/test.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var a11y = require('a11y');
+var chalk = require('chalk');
+var emoji = require('node-emoji');
+
+var pages = [
+    './meta-data/index.html',
+    './semantic-forms/index.html'
+];
+
+module.exports = function() {
+    pages.forEach(function(page) {
+        a11y(page, function (err, reports) {
+
+            if(err) throw err;
+
+            console.log(page);
+            console.log('------------------------------');
+
+            reports.audit.forEach(function (el) {
+                if (el.result === 'FAIL') {
+                    console.log(emoji.get(':cry:') + chalk.red( ' ' + el.heading));
+                } else {
+                    console.log(emoji.get(':smile:') + chalk.green(' ' + el.heading));
+                }
+            });
+
+            console.log('------------------------------');
+
+        });
+    });
+
+
+}();
